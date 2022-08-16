0afac95e19a5812e948f6bdd3136dbcc5dad3f2b
Nick <80983073+Kotliamba@users.noreply.github.com>
2022-08-16 15:24:49 +0300

    add initable

---
+++ Sources/Utils/Initable/CodeInitable.swift
@@ -0,0 +1,23 @@
+//
+//  CodeInitable.swift
+//
+//  Created by Николай on 16.08.2022.
+//
+
+import UIKit
+
+public protocol CodeInitable: Initable { }
+
+public extension CodeInitable where Self: UIViewController {
+
+    static func initiate() -> Self {
+        return Self()
+    }
+}
+
+public extension CodeInitable where Self: UIView {
+
+    static func initiate() -> Self {
+        return Self()
+    }
+}
--- Sources/Utils/Utils.swift
+++
@@ -1,6 +0,0 @@
-public struct Utils {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
