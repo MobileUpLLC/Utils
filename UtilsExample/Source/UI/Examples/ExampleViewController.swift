071b935f654f57a203584518e348e644b3dbe2bf
Vitaliy <vitaliy.vishnyakov@dev.mobileup.ru>
2022-08-16 15:08:47 +0300

    Add example project

---
+++ UtilsExample/Source/UI/Examples/ExampleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UtilsExample
+//
+//  Created by Николай on 16.08.2022.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
