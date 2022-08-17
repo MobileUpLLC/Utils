284cafcf4363ba95f57703373b2d9b9c4c86c462
Nick <80983073+Kotliamba@users.noreply.github.com>
2022-08-17 12:20:30 +0300

    Create LocalPushNotificationService.swift

---
+++ Sources/Utils/LocalPushNotification/LocalPushNotificationService.swift
@@ -0,0 +1,61 @@
+//
+//  File.swift
+//  
+//
+//  Created by Николай on 17.08.2022.
+//
+
+import UIKit
+
+public protocol LocalNotificationServiceDelegate: AnyObject {
+
+    func localNotificationService(
+        _ service: LocalNotificationService,
+        willPresent notification: UNNotification
+    ) -> UNNotificationPresentationOptions
+    
+    func localNotificationService(
+        _ service: LocalNotificationService,
+        didReceive notification: UNNotificationResponse
+    )
+}
+
+open class LocalNotificationService: NSObject, UNUserNotificationCenterDelegate {
+
+    public static let shared = LocalNotificationService()
+
+    public weak var delegate: LocalNotificationServiceDelegate?
+
+    public func requestAuthorization(
+        options: UNAuthorizationOptions = [.alert, .sound, .badge],
+        completion: @escaping (Result<Bool, Error>) -> Void
+    ) { }
+
+    public func getAuthorizationStatus(completion: UNAuthorizationStatus) { }
+
+    public func openApplicaitonSettings() { }
+
+    private override init() {
+        super.init()
+    
+        UNUserNotificationCenter.current().delegate = self
+    }
+    
+    public func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+
+    }
+    
+    public func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        completionHandler()
+    }
+}
+
+
