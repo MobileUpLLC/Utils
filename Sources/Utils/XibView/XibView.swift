75fb1185a76877e1e629e38003cf6db3d448d508
Vitaliy <vitaliy.vishnyakov@dev.mobileup.ru>
2022-08-17 16:16:21 +0300

    Resolve conversation

--- Sources/Utils/XibView/XibView.swift
+++ Sources/Utils/XibView/XibView.swift
@@ -11,12 +11,10 @@ open class XibView: View, XibInitable {
     
     open var view: UIView?
     
-    static private var nibCache: [String: UINib] = [:]
-    
     open override func initSetup() {
         super.initSetup()
         
-        checkViewFromNib()
+        layoutView()
     }
     
     public static func initiate() -> Self {
@@ -26,28 +24,7 @@ open class XibView: View, XibInitable {
     private func layoutView() {
         guard let view = view else { return }
         
-        insertSubview(view, at: 0)
+        insertSubview(view, at: .zero)
         layoutSubview(view)
-    }
-    
-    private func checkViewFromNib() {
-        if let viewFromNib = loadViewFromNib() {
-            view = viewFromNib
-            layoutView()
-        } else {
-            view = self
-        }
-    }
-    
-    private func loadViewFromNib() -> UIView? {
-        let nibName = Self.xibName
-        var nib: UINib? = XibView.nibCache[nibName]
-        
-        if nib == nil {
-            nib = UINib(nibName: nibName, bundle: Bundle(for: XibView.self))
-            XibView.nibCache[nibName] = nib
-        }
-        
-        return nib?.instantiate(withOwner: self, options: nil).first as? UIView
     }
 }
