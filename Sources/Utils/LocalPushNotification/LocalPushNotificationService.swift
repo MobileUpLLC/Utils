d164dc5194bfc6b79899ce8c183f3667f854ca09
Nick <80983073+Kotliamba@users.noreply.github.com>
2022-08-17 13:06:01 +0300

    Update LocalPushNotificationService.swift

--- Sources/Utils/LocalPushNotification/LocalPushNotificationService.swift
+++ Sources/Utils/LocalPushNotification/LocalPushNotificationService.swift
@@ -26,14 +26,14 @@ open class LocalNotificationService: NSObject, UNUserNotificationCenterDelegate 
 
     public weak var delegate: LocalNotificationServiceDelegate?
 
-    public func requestAuthorization(
+    open func requestAuthorization(
         options: UNAuthorizationOptions = [.alert, .sound, .badge],
         completion: @escaping (Result<Bool, Error>) -> Void
     ) { }
 
-    public func getAuthorizationStatus(completion: UNAuthorizationStatus) { }
+    open func getAuthorizationStatus(completion: UNAuthorizationStatus) { }
 
-    public func openApplicaitonSettings() { }
+    open func openApplicaitonSettings() { }
 
     private override init() {
         super.init()
@@ -41,7 +41,7 @@ open class LocalNotificationService: NSObject, UNUserNotificationCenterDelegate 
         UNUserNotificationCenter.current().delegate = self
     }
     
-    public func userNotificationCenter(
+    open func userNotificationCenter(
         _ center: UNUserNotificationCenter,
         willPresent notification: UNNotification,
         withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
@@ -49,7 +49,7 @@ open class LocalNotificationService: NSObject, UNUserNotificationCenterDelegate 
 
     }
     
-    public func userNotificationCenter(
+    open func userNotificationCenter(
         _ center: UNUserNotificationCenter,
         didReceive response: UNNotificationResponse,
         withCompletionHandler completionHandler: @escaping () -> Void
