1aa24f1188faec7240bb610724489c3d9be0c7a9
Vitaliy <vitaliy.vishnyakov@dev.mobileup.ru>
2022-08-18 14:49:49 +0300

    Do refactoring

--- Sources/Utils/Extensions/UIKit/UICollectionView+Ext.swift
+++ Sources/Utils/Extensions/UIKit/UICollectionView+Ext.swift
@@ -9,8 +9,13 @@ import UIKit
 
 public extension UICollectionView {
     
+    private enum Constants {
+        
+        static let nibType: String = "nib"
+    }
+    
     func register(_ cellClass: UICollectionViewCell.Type) {
-        if Bundle.main.path(forResource: cellClass.reuseId, ofType: "nib") == nil {
+        if Bundle.main.path(forResource: cellClass.reuseId, ofType: Constants.nibType) == nil {
             register(cellClass, forCellWithReuseIdentifier: cellClass.reuseId)
         } else {
             register(UINib(nibName: cellClass.reuseId, bundle: .main), forCellWithReuseIdentifier: cellClass.reuseId)
--- Sources/Utils/Extensions/UIKit/UITableView+Ext.swift
+++ Sources/Utils/Extensions/UIKit/UITableView+Ext.swift
@@ -9,8 +9,13 @@ import UIKit
 
 public extension UITableView {
     
+    private enum Constants {
+        
+        static let nibType: String = "nib"
+    }
+    
     func register(_ cellClass: UITableViewCell.Type) {
-        if Bundle.main.path(forResource: cellClass.reuseId, ofType: "nib") == nil {
+        if Bundle.main.path(forResource: cellClass.reuseId, ofType: Constants.nibType) == nil {
             register(cellClass, forCellReuseIdentifier: cellClass.reuseId)
         } else {
             register(UINib(nibName: cellClass.reuseId, bundle: .main), forCellReuseIdentifier: cellClass.reuseId)
