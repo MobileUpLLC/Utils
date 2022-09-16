1f0e185cb017a93f4114e2bbae973e44cc29cb1f
Nick <80983073+Kotliamba@users.noreply.github.com>
2022-09-16 12:28:13 +0300

    add action sheet with custom actions

--- Sources/Utils/Services/DeveloperTools/DeveloperToolsService.swift
+++ Sources/Utils/Services/DeveloperTools/DeveloperToolsService.swift
@@ -6,26 +6,27 @@
 //
 
 import UIKit
+import PulseUI
 
 public final class DeveloperToolsService {
     
+    private enum Constants {
+        
+        static let alertTitle = "Debug"
+        static let alertMessage = "Please Select an Option"
+        static let showLogActionTitle = "Show Logs"
+        static let clearLogActionTitle = "Clear Logs"
+        static let dismissAlertTitle = "Dismiss"
+    }
+    
     public static var isEnabled = false
+    public static var customActions: [CustomDebugAction] = []
     static weak var customActionDelegate: DeveloperToolsCustomActionDelegate?
     
     public static func setup() {
         NotificationCenter.default.addObserver(forName: .deviceHaveBeenShaken, object: nil, queue: nil) { _ in
             
-            let toolsController = DeveloperToolsController.init(
-                nibName: "DeveloperToolsController",
-                bundle: Bundle.module
-            )
-            toolsController.modalPresentationStyle = .overCurrentContext
-            
-            UIApplication.shared.windows.first?.rootViewController?.present(
-                toolsController,
-                animated: false,
-                completion: nil
-            )
+            showActionSheet()
             
             DeveloperToolsLogger.logMessage(
                 "DeveloperToolsService",
@@ -34,6 +35,52 @@ public final class DeveloperToolsService {
             )
         }
     }
+    
+    private static func showActionSheet() {
+        let alert = UIAlertController(
+            title: Constants.alertTitle,
+            message: Constants.alertMessage,
+            preferredStyle: .actionSheet
+        )
+        
+        alert.addAction(UIAlertAction(title: Constants.showLogActionTitle, style: .default) { _ in
+            openLogController()
+        })
+        
+        alert.addAction(UIAlertAction(title: Constants.clearLogActionTitle, style: .default) { _ in
+            DeveloperToolsLogger.clearLogs()
+        })
+
+        for action in customActions {
+            alert.addAction(UIAlertAction(title: action.title, style: .default) { _ in
+                action.handler()
+            })
+        }
+        
+        alert.addAction(UIAlertAction(title: Constants.dismissAlertTitle, style: .cancel) { _ in
+            alert.dismiss(animated: true)
+        })
+
+        UIApplication.shared.windows.first?.rootViewController?.present(
+            alert,
+            animated: false,
+            completion: nil
+        )
+    }
+    
+    private static func openLogController() {
+        UIApplication.shared.windows.first?.rootViewController?.presentedViewController?.present(
+            PulseUI.MainViewController(),
+            animated: false,
+            completion: nil
+        )
+    }
+}
+
+public struct CustomDebugAction {
+    
+    let title: String
+    let handler: () -> Void
 }
 
 public extension Notification.Name {
