0d9f01d97cbd98bea9e5f3b806be2687aec66847
Vitaliy <vitaliy.vishnyakov@dev.mobileup.ru>
2022-09-28 10:49:36 +0300

    Do refactoring

--- UtilsExample/Source/UI/Initable/Storyboard/StoryboardInitableController.swift
+++ UtilsExample/Source/UI/Initable/Storyboard/StoryboardInitableController.swift
@@ -8,7 +8,4 @@
 import UIKit
 import Utils
 
-final class StoryboardInitableController: UIViewController, StoryboardInitable {
-
-    static var storyboardName: String { "StoryboardInitableController" }
-}
+final class StoryboardInitableController: UIViewController, StoryboardInitable { }
