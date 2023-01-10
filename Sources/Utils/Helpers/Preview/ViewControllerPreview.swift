ab4de0a632afe2505cb7eacd4ac77bab5ceca434
Nikolai Timonin <nikki.timonin@yandex.ru>
2023-01-10 14:58:12 +0300

    Add view/controller live preview utils

---
+++ Sources/Utils/Helpers/Preview/ViewControllerPreview.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nikolai Timonin on 10.01.2023.
+//
+
+import SwiftUI
+
+public struct ViewControllerPreview: UIViewControllerRepresentable {
+    
+    private let viewContrllerBulder: () -> UIViewController
+    
+    public init(builder: @escaping () -> UIViewController) {
+        self.viewContrllerBulder = builder
+    }
+    
+    public func makeUIViewController(context: Context) -> some UIViewController {
+        viewContrllerBulder()
+    }
+    
+    public func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) { }
+}
---
+++ Sources/Utils/Helpers/Preview/ViewPreview.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nikolai Timonin on 10.01.2023.
+//
+
+import SwiftUI
+
+public struct ViewPreview: UIViewRepresentable {
+    
+    private let viewBuilder: () -> UIView
+
+    public init(builder: @escaping () -> UIView) {
+        self.viewBuilder = builder
+    }
+
+    public func makeUIView(context: Context) -> some UIView {
+        viewBuilder()
+    }
+
+    public func updateUIView(_ uiView: UIViewType, context: Context) { }
+}
